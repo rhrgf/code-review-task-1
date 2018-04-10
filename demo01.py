45cf83b514274b6d4f16b9e1fa3125551ec2025e
chenjiongs <1318352167@qq.com>
2018-04-10 15:34:05 +0800

    add output

--- demo01.py
+++ demo01.py
@@ -9,7 +9,8 @@ with open('input.txt') as f:
         words+=line.strip().split(' ')
 
 words_count =  collections.Counter(words)
+words_count = sorted(words_count.items(),key=lambda x:x[1],reverse=True)
 with open('output.txt','w') as f:
-    for (key,value) in sorted(words_count.items(),key=lambda x:x[1],reverse=True):
+    for (key,value) in words_count:
         f.write(key+" "+str(value)+"\n")
         print(key+" "+str(value)+"\n")
