bc75f0e90f3a3219fc0440c4d50dc5fb1c189b0c
liosure <15013461086@163.com>
2018-04-11 08:49:11 +0800

    try commit

---
+++ word_count.py
@@ -0,0 +1,38 @@
+import re
+
+def count(str_i):
+    dict_out={}
+    l = len(str_i)
+    k = 0
+    for i in range(l):
+        flag=0
+        temp = ord(str_i[i])
+        if (temp<=122 and temp>=97):
+            continue
+        else:
+            if k == i:
+                k=i+1
+                continue
+            s_temp = str_i[k:i]
+            for key in dict_out.keys():
+                if key == s_temp:
+                    dict_out[key] += 1
+                    k = i+1
+                    flag=1
+                    break
+            if flag==0:
+                dict_out[s_temp] = 1
+                k = i+1
+
+    return dict_out
+
+def main():
+    f = open('words.txt','r')
+    str_i=f.read()
+    dict_count = count(str_i)
+    dict_fin = sorted(dict_count,key=lambda a:a[0],reverse=True)
+    l = len(dict_fin)
+    for i in range(l):
+        print(dict_fin[i],' ',dict_count[dict_fin[i]])
+main()
+input('')
