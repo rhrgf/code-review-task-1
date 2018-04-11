b33fcfa93ae60131f1795d4835501cfe60355893
liosure <15013461086@163.com>
2018-04-11 09:28:52 +0800

    debug

--- word_count.py
+++ word_count.py
@@ -7,12 +7,14 @@ def count(str_i):
     for i in range(l):
         flag=0
         temp = ord(str_i[i])
-        if (temp<=122 and temp>=97):
+        if (temp<=122 and temp>=97 and i!=l-1):
             continue
         else:
             if k == i:
                 k=i+1
                 continue
+            if i==l-1:
+                i=i+1
             s_temp = str_i[k:i]
             for key in dict_out.keys():
                 if key == s_temp:
@@ -30,9 +32,9 @@ def main():
     f = open('words.txt','r')
     str_i=f.read()
     dict_count = count(str_i)
-    dict_fin = sorted(dict_count,key=lambda a:a[0],reverse=True)
+    dict_fin = sorted(dict_count.items(),key = lambda a:a[1],reverse=True)
     l = len(dict_fin)
     for i in range(l):
-        print(dict_fin[i],' ',dict_count[dict_fin[i]])
+        print(dict_fin[i][0],' ',dict_fin[i][1])
 main()
 input('')
