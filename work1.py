77fb559fc0c4a5122a125e152ee08921c80fd2c9
ChloeCAT14 <17356438520@163.com>
2018-04-11 09:07:40 +0800

    work1
    徐sir大二学生常璨

---
+++ work1.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+path = 'D:/Python/exercise/words.txt'
+with open(path, 'r') as text:
+   words = text.read().split()
+   print(words)
+words_index = set(words)
+counts_dict = {index:words.count(index) for index in words_index}
+for word in sorted(counts_dict, key=lambda x: counts_dict[x],reverse = True);
+    print('{} -- {}'.format(word,counts_dict[word]))
