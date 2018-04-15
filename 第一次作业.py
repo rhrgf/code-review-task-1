29ed9888f1ee1803c3358e6e9f9619e904c8a4c0
1483769437 <38247304+1483769437@users.noreply.github.com>
2018-04-15 15:33:07 +0800

    修改完全版
    将函数分块，加入异常处理，解决了无法剥离换行符的问题

--- 第一次作业.py
+++ 第一次作业.py
@@ -1,29 +1,41 @@
-def main():
-    
+def readfile():
+    file_name=input("请输入文件名：")
     while(1):
         try:
             with open(file_name) as file:
-                data=file.read()
-                words=data.split()
+                words=''
+                lines=file.readlines()
+                for line in lines:
+                    line=line.replace('\n',' ')
+                    words=words+line
+            word=words.split()
             break
         except:
             file_name=input("文件名有误请再次输入：")
-            
+    return word
+
+def countword(words):
     word_name=[]
     word_freq=[]
-    
     for word in words:
         if word not in word_name:
             word_name.append(word)
             word_freq.append(1)
         else:
             word_freq[word_name.index(word)]+=1
+    return word_name,word_freq
 
+def printword(word_name,word_freq):
     for key in range(len(word_freq)):
         freq=max(word_freq)
         word=word_name[word_freq.index(freq)]
         print('{}\t{}'.format(word,freq))
         word_name.remove(word)
         word_freq.remove(freq)
+
+def main():
+  word=readfile()
+  word_name,word_freq=countword(word)
+  printword(word_name,word_freq)
 
 main()
