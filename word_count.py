805af770dc5accdb54e07d0c5c256878502f0752
pyk1998 <37787058+pyk1998@users.noreply.github.com>
2018-04-18 10:24:19 +0800

    代码分块

--- word_count.py
+++ word_count.py
@@ -1,19 +1,23 @@
-f = open("words.txt",'r')
-char = f.read(1)
-st = ''
-dict = {}
-while char != '':
-    while(char == ' ' or char == '\n'):
-        char = f.read(1)
-    while(char != ' ' and char != '\n' and char != ''):
-        st = st + char
-        char = f.read(1)
-    if(st != ''):
-        try:
-            dict[st] = dict[st] + 1
-        except:
-            dict[st] = 1
+def count(file):
+    char = f.read(1)
     st = ''
-dict_sorted = sorted(dict.items(),key=lambda item:item[1],reverse=True);
-for item in dict_sorted:
-    print(str(item[0])+' '+str(item[1]))
+    dict = {}
+    while char != '':
+        while (char == ' ' or char == '\n'):
+            char = f.read(1)
+        while (char != ' ' and char != '\n' and char != ''):
+            st = st + char
+            char = f.read(1)
+        if (st != ''):
+            if(st in dict):
+                dict[st] = dict[st] + 1
+            else:
+                dict[st] = 1
+        st = ''
+    return dict
+if __name__ == '__main__':
+    f = open("words.txt", 'r')
+    dict = count(f)
+    dict_sorted = sorted(dict.items(), key=lambda item: item[1], reverse=True);
+    for item in dict_sorted:
+        print(str(item[0]) + ' ' + str(item[1]))
